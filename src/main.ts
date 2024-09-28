d620cb518860d87cc9f8d575610402f5d6ecc903
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2024-09-28 14:57:28 +0100

    Setup p5

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-import './style.css'
+import P5 from "p5";
 
-console.log("HELLO")
+import "./style.css";
+
+function sketch(p5: P5) {
+  console.log("HELLO", p5);
+
+  p5.setup = () => {
+    p5.createCanvas(800, 600);
+
+    // p5.background("red")
+
+    p5.stroke(255, 255, 0);
+    p5.line(30, 20, 200, 200);
+  };
+
+  p5.draw = () => {};
+}
+
+new P5(sketch);
