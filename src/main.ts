f021c409345f5bd051f7932c8644c943455af95e
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2024-09-28 15:13:39 +0100

    Add circle example

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import P5 from "p5";
 import "./style.css";
 
 function sketch(p5: P5) {
-  console.log("HELLO", p5);
-
   p5.setup = () => {
     p5.createCanvas(800, 600);
 
-    // p5.background("red")
-
-    p5.stroke(255, 255, 0);
-    p5.line(30, 20, 200, 200);
+    p5.stroke(255, 0, 0);
+    p5.strokeWeight(2);
+    p5.noCursor();
   };
 
-  p5.draw = () => {};
+  p5.draw = () => {
+    p5.background("white");
+    p5.circle(p5.mouseX, p5.mouseY, 50 + (p5.sin(p5.frameCount / 50) * 100));
+  };
 }
 
 new P5(sketch);
